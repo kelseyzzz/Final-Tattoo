9620656b4f50cb3fa911385148a806854368cd22
Kelsey Z <you@example.comzahandke@gmail.com>
2017-06-07 17:20:54 -0400

    moved file

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+(function() {
+
+	window.App = window.App || {};
+	
+	var i = 0;
+
+	i += 1;
+
+	console.log('i = ' +i);
+
+}());
+
+
+// $(document).ready(function(){
+//     $(".artist-info").hover(function(){
+//         $(".artist-img").fadeIn()
+//     });
+// });
